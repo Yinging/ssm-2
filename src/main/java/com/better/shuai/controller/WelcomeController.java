9db156d320ec4d708f9ca8582a84013d8fc775ba
xiashuai <shuaixiaa@icloud.com>
2016-09-09 17:34:49 +0800

    change

--- src/main/java/com/better/shuai/controller/WelcomeController.java
+++ src/main/java/com/better/shuai/controller/WelcomeController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
